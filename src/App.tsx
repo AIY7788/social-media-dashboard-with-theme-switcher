379b33414be54c0628fdd7984c28e6e258856d59
AIY7788 <aiykhounnakhon@gmail.com>
2026-02-04 13:08:33 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './styles/App.scss'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>Social Media Dashboard</h1>
+        <p>Total Followers: 23,004</p>
+        <hr/>
+      </header>
+    </>
+  )
+}
+
+export default App

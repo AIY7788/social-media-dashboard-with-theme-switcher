2f423cb6723dc7aa8bf8013a93b343b460d6bcf5
AIY7788 <aiykhounnakhon@gmail.com>
2026-02-08 10:43:02 +0700

    Separate data into own file

--- src/assets/data/overviews.mock.ts
+++ src/assets/data/overviews.mock.ts
@@ -1,46 +1,7 @@
-import iconFacebook from "./images/icon-facebook.svg";
-import iconInstagram from "./images/icon-instagram.svg";
-import iconTwitter from "./images/icon-twitter.svg";
-import iconYoutube from "./images/icon-youtube.svg";
-
-export const socialStats = [
-  {
-    platform: "facebook",
-    icon: iconFacebook,
-    username: "@nathanf",
-    metric: "followers",
-    total: 1_987,
-    change: 12,
-    trend: "up",
-  },
-  {
-    platform: "twitter",
-    icon: iconTwitter,
-    username: "@nathanf",
-    metric: "followers",
-    total: 1_044,
-    change: 99,
-    trend: "up",
-  },
-  {
-    platform: "instagram",
-    icon: iconInstagram,
-    username: "@realnathanf",
-    metric: "followers",
-    total: 11_000,
-    change: 1_099,
-    trend: "up",
-  },
-  {
-    platform: "youtube",
-    icon: iconYoutube,
-    username: "Nathan F.",
-    metric: "subscribers",
-    total: 8_239,
-    change: 144,
-    trend: "down",
-  },
-];
+import iconFacebook from "@/assets/images/icon-facebook.svg";
+import iconInstagram from "@/assets/images/icon-instagram.svg";
+import iconTwitter from "@/assets/images/icon-twitter.svg";
+import iconYoutube from "@/assets/images/icon-youtube.svg";
 
 export const overviewStats = [
   {
